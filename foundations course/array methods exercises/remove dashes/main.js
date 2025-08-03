e4d3c8f840acfb2bba3e79feb45040d9af115ef6
julio96correa <julio96correa@gmail.com>
2025-08-03 16:15:27 -0500

    feat: add camelize function solution

---
+++ foundations course/array methods exercises/remove dashes/main.js
@@ -0,0 +1,21 @@
+/**
+ Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+That is: removes all dashes, each word after dash becomes uppercased.
+
+Examples:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+ */
+
+function camelize(word){
+    return word
+    .split('-')
+    .map(
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    ).join('')
+}
+
+console.log(camelize("hola-mundo-feroz"))
